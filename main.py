62d060c16b53db3fb7121457f161e807495a5a12
DIPUTAT <126963464+DIPUTAT@users.noreply.github.com>
2025-05-06 20:53:26 +0300

    elem

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#import spacy
 import ru_core_news_md
 from spacy.lang.ru.stop_words import STOP_WORDS
 
 model = ru_core_news_md.load()
 text = model("Ставки на спор, большые выйграши, быстрые выплаты, надежный букмекер")
-text_list = [word for word in text]
+text_list = [word.lemma_ for word in text]
 print(text_list)
+
+filter_text_list = [elem for elem in text_list if not elem in STOP_WORDS]
+print(filter_text_list)
+
